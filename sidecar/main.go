7071524f2e364da130eb82375cc560d2cf0b3f7c
Beñat Gartzia Arruabarrena <bgartzia@redhat.com>
2026-04-28 16:54:27 +0200

    sidecar: Remove redundant log
    Signed-off-by: Beñat Gartzia Arruabarrena <bgartzia@redhat.com>

--- sidecar/main.go
+++ sidecar/main.go
@@ -45,7 +45,6 @@ func main() {
 	socket, err := net.Listen("unix", socketPath)
 	if err != nil {
 		log.Log.Reason(err).Errorf("Failed to initialized socket on path: %s", socket)
-		log.Log.Error("Check whether given directory exists and socket name is not already taken by other file")
 		os.Exit(1)
 	}
 	defer os.Remove(socketPath)
