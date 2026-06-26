1f09a25dc33115b301aa5279ebd9047af8dac3c6
Beñat Gartzia Arruabarrena <bgartzia@redhat.com>
2026-06-26 11:05:52 +0200

    vendor: Dependencies for e2e tests
    Most of the dependencies that have been added are kubevirt.io/kubevirt:
        - kubevirt.io/kubevirt/tests/libmigration
        - kubevirt.io/kubevirt/tests/libvmops
        - kubevirt.io/kubevirt/tests/libvmifact
        - kubevirt.io/kubevirt/pkg/libvmi
        - kubevirt.io/client-go/kubecli
    
    Which bring in a few more as its own.
    
    Signed-off-by: Beñat Gartzia Arruabarrena <bgartzia@redhat.com>

---
+++ vendor/kubevirt.io/kubevirt/pkg/monitoring/rules/alerts/virt-api.go
@@ -0,0 +1,87 @@
+/*
+ * This file is part of the KubeVirt project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright The KubeVirt Authors.
+ */
+
+package alerts
+
+import (
+	"fmt"
+
+	promv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
+	"k8s.io/utils/ptr"
+)
+
+func virtAPIAlerts(namespace string) []promv1.Rule {
+	return []promv1.Rule{
+		{
+			Alert: "VirtAPIDown",
+			Expr:  intstr.FromString("kubevirt_virt_api_up == 0"),
+			For:   ptr.To(promv1.Duration("10m")),
+			Annotations: map[string]string{
+				summaryAnnotationKey: "All virt-api servers are down.",
+			},
+			Labels: map[string]string{
+				severityAlertLabelKey:        "critical",
+				operatorHealthImpactLabelKey: "critical",
+			},
+		},
+		{
+			Alert: "LowVirtAPICount",
+			Expr: intstr.FromString(fmt.Sprintf(
+				"kubevirt_virt_api_up / on() kube_deployment_spec_replicas{deployment='virt-api', namespace='%s'} < 0.75", namespace,
+			)),
+			For: ptr.To(promv1.Duration("60m")),
+			Annotations: map[string]string{
+				summaryAnnotationKey: "Less than 75% of desired virt-api pods are running.",
+			},
+			Labels: map[string]string{
+				severityAlertLabelKey:        "warning",
+				operatorHealthImpactLabelKey: "warning",
+			},
+		},
+		{
+			Alert: "VirtApiRESTErrorsBurst",
+			Expr:  intstr.FromString(getErrorRatio(namespace, "virt-api", "(4|5)[0-9][0-9]", fiveMinutes) + " >= 0.8"),
+			For:   ptr.To(promv1.Duration("5m")),
+			Annotations: map[string]string{
+				summaryAnnotationKey: getRestCallsFailedWarning(eightyPercent, "virt-api", fiveMinutes),
+			},
+			Labels: map[string]string{
+				severityAlertLabelKey:        "critical",
+				operatorHealthImpactLabelKey: "critical",
+			},
+		},
+		{
+			Alert: "KubeVirtDeprecatedAPIRequested",
+			Expr: intstr.FromString(
+				"sum by (resource,group,version) ((round(increase(kubevirt_api_request_deprecated_total{verb!~\"LIST|WATCH|DELETE\"}[10m])) > 0 and " +
+					" kubevirt_api_request_deprecated_total{verb!~\"LIST|WATCH|DELETE\"} offset 10m) or " +
+					" (kubevirt_api_request_deprecated_total{verb!~\"LIST|WATCH|DELETE\"} != 0 " +
+					" unless kubevirt_api_request_deprecated_total{verb!~\"LIST|WATCH|DELETE\"} offset 10m))",
+			),
+			Annotations: map[string]string{
+				descriptionAnnotationKey: "Detected requests to the deprecated {{ $labels.resource }}.{{ $labels.group }}/{{ $labels.version }} API.",
+				summaryAnnotationKey:     "Detected {{ $value }} requests in the last 10 minutes.",
+			},
+			Labels: map[string]string{
+				severityAlertLabelKey:        "info",
+				operatorHealthImpactLabelKey: "none",
+			},
+		},
+	}
+}
